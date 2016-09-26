c1a6903b3da0c7dcbbe8e6d4da9421f1a53ef143
Thomas Quintana <quintana.thomas@gmail.com>
2016-09-26 19:52:37 -0400

    working on actor framework

--- jobber/actors/actor_ref.py
+++ jobber/actors/actor_ref.py
@@ -21,8 +21,9 @@ import logging
 
 from uuid import uuid4
 
-from exceptions import InterruptException
-from executable import Task
+from jobber.actors.exceptions import InterruptException
+from jobber.actors.executable import Task
+from jobber.actors.messages.poison_pill import PoisonPill
 
 class ActorRef(Task):
   '''
@@ -40,28 +41,59 @@ class ActorRef(Task):
     self._mailbox = list()
     self._path = path
     self._scheduler = scheduler
+    self._state = Task.Idle
     self._urn = uuid4()
+    # NOTE: This is typically bad and should make your linter complain.
+    # Monkey patch the actor.
+    self._actor._actor_ref = self
+    # Schedule ourselves for execution.
+    self._scheduler.schedule(self)
 
   def __getattr__(self, name):
     if name == "path":
       return self._path.geturl()
+    elif name == "state":
+      return self._state
     elif name == "urn":
       return self._urn
 
   def run(self):
-    while len(self._mailbox) > 0:
-      message = self._mailbox.pop(0)
-      try:
-        self._actor.receive(message)
-      except Exception as exception:
-        self._logger.exception(exception)
-      # Try to return control of the processor to the scheduler if
-      # the scheduler fires an InterruptException we hand over control
-      # and if it doesn't we keep processing messages.
-      try:
-        self._scheduler.interrupt()
-      except InterruptException:
+    while True:
+      if len(self._mailbox) == 0:
+        self._state = Task.Idle
         break
+      else:
+        self._state = Task.Running
+        # Take a message from the head of the queue.
+        message = self._mailbox.pop(0)
+        # If we get a poison pill we must die.
+        if isinstance(message, PoisonPill):
+          self._scheduler.unschedule(self)
+          self._state = Task.Completed
+          # If the actor defined a stop method now is a good
+          # time to call it.
+          if hasattr(self._actor, "stop"):
+            if callable(self._actor.stop):
+              self._actor.stop()
+          break
+        # Process an incoming message.
+        try:
+          self._actor.receive(message)
+        except Exception as exception:
+          self._logger.exception(exception)
+        # Try to return control of the processor to the scheduler if
+        # the scheduler fires an InterruptException we hand over control
+        # and if it doesn't we keep processing messages.
+        try:
+          self._scheduler.interrupt()
+        except InterruptException:
+          if len(self._mailbox) > 0:
+            self._state = Task.Ready
+          else:
+            self._state = Task.Idle
+          break
 
   def tell(self, message):
     self._mailbox.append(message)
+    if self._state == Task.Idle:
+      self._state = Task.Ready
